4f14454347a63faac1536e40c84a07828cbbbee1
MeghaN28 <n.megha82@gmail.com>
2026-04-09 22:34:47 -0700

    BinarySearch 1

--- SearchinMatrix.java
+++ SearchinMatrix.java
@@ -12,36 +12,29 @@
 // We can then compare the mid value with the target and adjust 
 // the low and high pointers accordingly until we find the target or the search space is empty.
 
-public class SearchinMatrix {
-    class Solution {
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        int rows=matrix.length;
-        int cols=matrix[0].length;
+        int left = 0;
+        int right = m * n - 1;
 
-        int low=0;
-        int high=rows*cols-1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            int midval=matrix[mid/cols][mid%cols]; //we get index convert it back to row*column 
+            int row = mid / n;
+            int col = mid % n;
 
-            if(midval==target)
-            {
+            if (matrix[row][col] == target) {
                 return true;
-            }
-            if(midval<target)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
+            } else if (matrix[row][col] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+
         return false;
-        
     }
-}
 }
